453c8e85477284185f5e12a9c9a6af369d608c2b
Bharat Kathi <bharat1031@gmail.com>
2024-01-26 22:14:39 -0800

    hand coords send over ws

--- server/main.py
+++ server/main.py
@@ -8,8 +8,6 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     server = initialize_socket(app)
-    # server_thread = threading.Thread(target=server.run, args=(app,), kwargs={'port': 30000})
-    camera_thread = threading.Thread(target=start_camera)
-    # server_thread.start()
-    camera_thread.start()
-    # start_camera()
+    server_thread = threading.Thread(target=server.run, args=(app,), kwargs={'port': 30000})
+    server_thread.start()
+    start_camera()
