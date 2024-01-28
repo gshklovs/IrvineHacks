144baac53c952dbae8f51c536e7bace58d4ee910
Rohan Virmani <ro.han@berkeley.edu>
2024-01-27 21:34:07 -0800

    got training to work need to fix camera slash main slash app.py

--- server/main.py
+++ server/main.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 from socket_service import *
 from camera import *
+from grmodel import main
 from dotenv import load_dotenv
+import os
 
 # ====== ENV VARIABLES ====== #
 load_dotenv()
@@ -12,12 +14,15 @@ if port == None:
     port = 30000
 # ============================ #
 
+app_thread = threading.Thread(target=main) #create the new mf thread
+app_thread.start # Start a new thread
+
 app = Flask(__name__)
 
 if __name__ == '__main__':
     server = initialize_socket(app)
     server_thread = threading.Thread(target=server.run, args=(app,), kwargs={'port': int(port)})
     server_thread.start()
-    coordinate_thread = threading.Thread(target=send_coordinates)
-    coordinate_thread.start()
-    start_camera()
+    # coordinate_thread = threading.Thread(target=send_coordinates)
+    # coordinate_thread.start()
+    # start_camera()
